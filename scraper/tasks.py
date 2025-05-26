9cb037f91d305246c283255c10d9934c14e1dfb2
vivek singh <viveksingh00747@gmail.com>
2025-05-27 00:37:59 +0530

    added jobs to tasks

--- scraper/tasks.py
+++ scraper/tasks.py
@@ -14,68 +14,22 @@ celery_app = Celery(
 celery_app.config_from_object("celeryconfig")
 
 
-jobs = [
-    {
-        "first_name": "Vivek",
-        "last_name": "Singh",
-        "username": "viveksingh",
-        "email": "viveksingh00747@gmail.com",
-        "password": "password",
-    },
-    {
-        "first_name": "John",
-        "last_name": "Doe",
-        "username": "johndoe",
-        "email": "johndoe@example.com",
-        "password": "secret123",
-    },
-]
-
-
 # jobs = [
 #     {
-#         "title": "Python Developer",
-#         "company": "Eviden",
-#         "location": "Gurgaon, Haryana, India",
-#         "description": "We are looking for a Python Developer with experience in building high-performing, scalable, enterprise-grade applications. You will be part of a talented software team that works on mission-critical applications.",
-#         "salary": "₹10,00,000 - ₹20,00,000 a year",
-#         "posted_at": "2023-10-01",
-#         "type": "Full-time",
+#         "first_name": "Vivek",
+#         "last_name": "Singh",
+#         "username": "viveksingh",
+#         "email": "viveksingh00747@gmail.com",
+#         "password": "password",
 #     },
 #     {
-#         "title": "Full Stack Developer",
-#         "company": "Microsoft",
-#         "location": "Bengaluru, Karnataka, India",
-#         "description": "We are looking for a Full Stack Developer with expertise in both front-end and back-end technologies. You will be responsible for developing and maintaining web applications that provide a seamless user experience.",
-#         "salary": "₹12,00,000 - ₹22,00,000 a year",
-#         "posted_at": "2023-10-02",
-#         "type": "Full-time",
+#         "first_name": "John",
+#         "last_name": "Doe",
+#         "username": "johndoe",
+#         "email": "johndoe@example.com",
+#         "password": "secret123",
 #     },
 # ]
-
-
-@celery_app.task
-def insert_jobs():
-    db = SessionLocal()
-    try:
-        for job in jobs:
-            user = User(
-                first_name=job["first_name"],
-                last_name=job["last_name"],
-                username=job["username"],
-                email=job["email"],
-                password=job["password"],
-                created_at=datetime.utcnow(),
-                updated_at=datetime.utcnow(),
-            )
-            db.add(user)
-        db.commit()
-        print("Users inserted")
-    except Exception as e:
-        db.rollback()
-        print(f"Insert failed: {e}")
-    finally:
-        db.close()
 
 
 # @celery_app.task
@@ -83,23 +37,68 @@ def insert_jobs():
 #     db = SessionLocal()
 #     try:
 #         for job in jobs:
-#             job = Job(
+#             user = User(
 #                 first_name=job["first_name"],
 #                 last_name=job["last_name"],
 #                 username=job["username"],
 #                 email=job["email"],
 #                 password=job["password"],
-#                 created_at=datetime.now(timezone.utc),
-#                 updated_at=datetime.now(timezone.utc),
+#                 created_at=datetime.utcnow(),
+#                 updated_at=datetime.utcnow(),
 #             )
-#             db.add(job)
+#             db.add(user)
 #         db.commit()
-#         print("Job inserted")
+#         print("Users inserted")
 #     except Exception as e:
 #         db.rollback()
 #         print(f"Insert failed: {e}")
 #     finally:
 #         db.close()
+
+
+jobs = [
+    {
+        "title": "Python Developer",
+        "company": "Eviden",
+        "location": "Gurgaon, Haryana, India",
+        "description": "We are looking for a Python Developer with experience in building high-performing, scalable, enterprise-grade applications. You will be part of a talented software team that works on mission-critical applications.",
+        "salary": "₹10,00,000 - ₹20,00,000 a year",
+        "type": "Full-time",
+    },
+    {
+        "title": "Full Stack Developer",
+        "company": "Microsoft",
+        "location": "Bengaluru, Karnataka, India",
+        "description": "We are looking for a Full Stack Developer with expertise in both front-end and back-end technologies. You will be responsible for developing and maintaining web applications that provide a seamless user experience.",
+        "salary": "₹12,00,000 - ₹22,00,000 a year",
+        "type": "Full-time",
+    },
+]
+
+
+@celery_app.task
+def insert_jobs():
+    db = SessionLocal()
+    try:
+        for job in jobs:
+            job = Job(
+                title=job["title"],
+                company=job["company"],
+                location=job["location"],
+                description=job["description"],
+                salary=job["salary"],
+                type=job["type"],
+                posted_at=datetime.now(timezone.utc),
+                # updated_at=datetime.now(timezone.utc), created a created_at field in the model
+            )
+            db.add(job)
+        db.commit()
+        print("Job inserted")
+    except Exception as e:
+        db.rollback()
+        print(f"Insert failed: {e}")
+    finally:
+        db.close()
 
 
 @celery_app.task
--- shared/scraperdb.py
+++ shared/scraperdb.py
@@ -8,6 +8,5 @@ from dotenv import load_dotenv
 env_path = Path("/.env")
 load_dotenv(dotenv_path=env_path)
 SCRAPER_DATABASE_URL = os.getenv("SCRAPER_DATABASE_URL")
-print("sssssssssssssssssssssssssss", SCRAPER_DATABASE_URL)
 engine = create_engine(SCRAPER_DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(bind=engine)
